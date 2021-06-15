0bf3d4ce8101f16b260e9d8eb39757f9aeb8f52c
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-15 18:13:50 -0300

    Add Tests for compareTo Method of BaseModel

--- src/test/java/models/BaseModelTest.java
+++ src/test/java/models/BaseModelTest.java
@@ -8,6 +8,39 @@ class Model1 extends BaseModel { }
 
 class BaseModelTest {
 	@Test
+	void testCompareToSmaller() {
+		BaseModel entity1 = new Model1();
+		BaseModel entity2 = new Model1();
+		
+		entity1.setId(1);
+		entity2.setId(2);
+		
+		assertTrue(entity2.compareTo(entity1) > 0);
+	}
+	
+	@Test
+	void testCompareToEquals() {
+		BaseModel entity1 = new Model1();
+		BaseModel entity2 = new Model1();
+		
+		entity1.setId(1);
+		entity2.setId(1);
+		
+		assertTrue(entity1.compareTo(entity2) == 0);
+	}
+	
+	@Test
+	void testCompareToGreater() {
+		BaseModel entity1 = new Model1();
+		BaseModel entity2 = new Model1();
+		
+		entity1.setId(1);
+		entity2.setId(2);
+		
+		assertTrue(entity1.compareTo(entity2) < 0);
+	}
+
+	@Test
 	void testEqualsBaseModelWithEqual() {
 		BaseModel entity1 = new Model1();
 		BaseModel entity2 = new Model1();
