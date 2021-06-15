cbf1567284329001e601034a2fbe7d4da2d04d21
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-15 13:23:50 -0300

    Create BaseModel

---
+++ src/main/java/models/BaseModel.java
@@ -0,0 +1,16 @@
+package models;
+
+/**
+ * @author Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
+ *
+ */
+public abstract class BaseModel {
+	/**
+	 * Unique identifier
+	 */
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+}
