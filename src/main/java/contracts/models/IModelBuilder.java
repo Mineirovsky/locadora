4ff93dbb17bf462c3511c6004cda234c659790dc
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-22 15:46:52 -0300

    Create IModelBuilder Interface

---
+++ src/main/java/contracts/models/IModelBuilder.java
@@ -0,0 +1,15 @@
+package contracts.models;
+
+public interface IModelBuilder<T extends IModel> {
+	/**
+	 * Set a field in the model
+	 *
+	 * @param field
+	 * @param value
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws IllegalArgumentException
+	 */
+	public IModelBuilder<T> set (String field, Object value) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException;
+	public T build();
+}
