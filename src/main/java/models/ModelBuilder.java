74c19fcbf26ee8458e4d43a21028f0731ec1c3d4
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-22 15:47:13 -0300

    Implement ModelBuilder

---
+++ src/main/java/models/ModelBuilder.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.lang.reflect.Field;
+
+import contracts.models.IModelBuilder;
+
+public class ModelBuilder<T extends BaseModel> implements IModelBuilder<T> {
+	private T model;
+
+	public ModelBuilder (T model) {
+		this.model = model;
+	}
+
+	@Override
+	public IModelBuilder<T> set(String fieldName, Object value)
+		throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+	{
+		Field field;
+		
+		if ((field = findField(fieldName)) == null) {
+			throw new NoSuchFieldException(fieldName + " is not a member of the model " + model.getClass().getName());
+		}
+		
+		field.set(model, value);
+		
+		return this;
+	}
+
+	@Override
+	public T build() {
+		return model;
+	}
+
+	private Field findField(String fieldName) {
+		for (Field field: model.getFields()) {
+			if (field.getName().equals(fieldName)) {
+				return field;
+			}
+		}
+		
+		return null;
+	}
+	
+	public final String[] getFields() {
+		Field[] fields = model.getFields();
+		String[] fieldNames = new String[fields.length];
+		
+		for (int i = 0; i < fields.length; i++) {
+			fieldNames[i] = fields[i].getName();
+		}
+		
+		return fieldNames;
+	}
+}
