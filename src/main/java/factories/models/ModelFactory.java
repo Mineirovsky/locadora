9033e118f2ee1a1fbcf48acdb7069ae454ff04b4
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-23 13:09:07 -0300

    Implement ModelFactory Abstract Class

---
+++ src/main/java/factories/models/ModelFactory.java
@@ -0,0 +1,24 @@
+package factories.models;
+
+import contracts.factories.IModelFactory;
+import models.BaseModel;
+import models.ModelBuilder;
+
+public abstract class ModelFactory<T extends BaseModel> implements IModelFactory<T> {
+	protected ModelBuilder<T> getBuilder() {
+		return new ModelBuilder<T>(create());
+	}
+	
+	@Override
+	public T create(int id) {
+		ModelBuilder<T> builder = getBuilder();
+
+		try {
+			builder.set("id", id);
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			// No need to handle these exceptions
+		}
+		
+		return builder.build();
+	}
+}
