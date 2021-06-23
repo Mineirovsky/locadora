380fd65a28a7f8eb105d5b3b9272865a995be0f8
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-22 23:14:52 -0300

    Remove Invisible Trailing Chars From Csv Help

--- src/main/java/helpers/Csv.java
+++ src/main/java/helpers/Csv.java
@@ -3,21 +3,21 @@ package helpers;
 public class Csv {
 	public static boolean hasOffendingChar(String text) {
 		final String[] offendingChars = { ",", "\"", "\n" };
-		
+
 		for (String offendingChar: offendingChars) {
 			if (text.contains(offendingChar)) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	public static String escape(String text) {
 		if (hasOffendingChar(text)) {
 			return "\"" + text.replace("\"", "\"\"") + "\"";
 		}
-		
+
 		return text;
 	}
 }
