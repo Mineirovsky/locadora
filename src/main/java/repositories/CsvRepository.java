66998fe20c405e4020373b2c96d65d857ded43e1
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-06-29 11:11:01 -0300

    Add Dependencies to CsvRepository

--- src/main/java/repositories/CsvRepository.java
+++ src/main/java/repositories/CsvRepository.java
@@ -1,18 +1,26 @@
 package repositories;
 
+import java.util.SortedSet;
 import contracts.drivers.IStorage;
+import contracts.factories.IModelFactory;
 import contracts.repositories.IRepository;
+import helpers.CsvParser;
 import models.BaseModel;
 
 public abstract class CsvRepository<T extends BaseModel> implements IRepository<T> {
 	protected IStorage storage;
+	protected IModelFactory<T> factory;
+	CsvParser<T> parser;
+	protected SortedSet<T> items;
 
-	public CsvRepository(IStorage storage) {
+	public CsvRepository(IStorage storage, IModelFactory<T> factory, CsvParser<T> parser) {
 		this.storage = storage;
+		this.factory = factory;
+		this.parser = parser;
 	}
-	
-	protected abstract String getName();
-	
+
+	abstract protected String getName();
+
 	protected String getFileName() {
 		return getName() + ".csv";
 	}
